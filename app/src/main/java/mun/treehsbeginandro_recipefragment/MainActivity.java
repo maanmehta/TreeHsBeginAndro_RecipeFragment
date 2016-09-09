d4bf5501198ffb90636371b78472d833dad79d51
maanmehta <msmgrd-adm@yahoo.com>
2016-09-09 10:52:41 -0400

    commit before selling zenbook

--- app/src/main/java/mun/treehsbeginandro_recipefragment/MainActivity.java
+++ app/src/main/java/mun/treehsbeginandro_recipefragment/MainActivity.java
@@ -4,18 +4,35 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecipeListFragment.OnRecipeSelectedInterface{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecipeListFragment myFragment = new RecipeListFragment();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.placeHolder,myFragment);
-        fragmentTransaction.commit();
+        // only instantiate a new fragment if it does nto exist already so it is not created on
+        // top of existing fragment when you rotate the screen on whenever onCreate is called
+        // and another already exists
+
+        RecipeListFragment savedFragment = (RecipeListFragment) getFragmentManager().findFragmentById(R.id.placeHolder);
+        if (savedFragment == null) {
+            RecipeListFragment myFragment = new RecipeListFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.placeHolder,myFragment);
+            fragmentTransaction.commit();
+        }
+
+
+    }
+
+    @Override
+    public void onListRecipeSelected(int position) {
+
+        Toast.makeText(MainActivity.this, Recipes.directions[position], Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/mun/treehsbeginandro_recipefragment/RecipeListFragment.java
+++ app/src/main/java/mun/treehsbeginandro_recipefragment/RecipeListFragment.java
@@ -3,15 +3,39 @@ package mun.treehsbeginandro_recipefragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class RecipeListFragment extends Fragment {
+
+    public interface OnRecipeSelectedInterface {
+        public void onListRecipeSelected(int position);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list,container,false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.ListRecyclerView);
+
+        // get an instance of this Fragments Activity and cast it to our Interface as the Activity
+        // implements our interface. In other words the Activity is the listener
+        // then pass the listener to the Adapter. Remember the onClick handler method that will be
+        // automatically called when the list item view is clicked is in the ViewHolder class with
+        // in the Adapter class. So we pass in the listener object (activity) to the adapter
+        // and then onClick method of the ViewHolder calls the onListRecipeSelected of Activity
+        // where we show the Toast. This is how we communicate a click or touch event from a fragment
+        // to an Activity as it is Activity that is handling the UI
+        OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface)getActivity();
+
+        ListAdapter listAdapter = new ListAdapter(listener);
+        recyclerView.setAdapter(listAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
 
         return view;
     }
